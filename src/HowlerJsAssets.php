b7ee3ff51c760a5c84cfc44fa45ff55ecaf5fe74
Luis Gonzalez <luisarmando1234@gmail.com>
2018-10-19 15:11:13 -0500

    Create HowlerJsAssets.php

---
+++ src/HowlerJsAssets.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gogl92
+ * Date: 19/10/18
+ * Time: 11:07 AM
+ */
+
+namespace inquid\howlerjs;
+
+
+use yii\web\AssetBundle;
+
+class HowlerJsAssets extends AssetBundle
+{
+    public $sourcePath = '@vendor/inquid/yii2-howler-js/src/assets/';
+    public $css = ['css/player.css'];
+    public $js = ['js/player','js/siriwave','https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.15/howler.min.js'];
+    public $depends = ['yii\web\JqueryAsset'];
+}
