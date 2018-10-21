15e0543bd6440691b30fcef6b8fc405452ccf442
gogl92 <luisarmando1234@gmail.com>
2018-10-20 20:37:06 -0500

    update

--- src/HowlerJsAssets.php
+++ src/HowlerJsAssets.php
@@ -13,8 +13,9 @@ use yii\web\AssetBundle;
 
 class HowlerJsAssets extends AssetBundle
 {
-    public $sourcePath = '@vendor/inquid/yii2-howler-js/src/assets/';
+    public $sourcePath = '@vendor/inquid/yii2-howler-js/src/assets';
     public $css = ['css/player.css'];
-    public $js = ['js/player','js/siriwave','https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.15/howler.min.js'];
+
+    public $js = ['https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.15/howler.min.js','js/siriwave.js'];
     public $depends = ['yii\web\JqueryAsset'];
 }
